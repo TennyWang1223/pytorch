5bdd968acf5d7f8c9fbf3eb67eeae6307182f505
Akash Verma <Akash.Verma3@amd.com>
2025-03-18 16:18:08 +0000

    Added the code to download CK library from compute-artifactory and create link target.

--- cmake/External/ck.cmake
+++ cmake/External/ck.cmake
@@ -1,10 +1,22 @@
 #
-# create INTERFACE target for CK shared library
+# create INTERFACE target for CK library
 #
+
+# set source and destination
+set(SOURCE_URL "https://compute-artifactory.amd.com/artifactory/rocm-generic-local/torch_ck_gen_lib/ck_8086bbe3a78d931eb96fe12fdc014082e18d18d3/rocm_6.2.0/torch_2.5/libck_kernels.so")
+set(DESTINATION "${CMAKE_SOURCE_DIR}/torch/lib/libck_kernels.so")
+
+# download CK library
+file(DOWNLOAD ${SOURCE_URL} ${DESTINATION} SHOW_PROGRESS STATUS download_status)
+if(NOT download_status)
+	message(STATUS "Downloaded CK library successfully.")
+else()
+	message(FATAL_ERROR "Failed to download the CK library from ${SOURCE_URL}.")
+endif()
 
 # create INTERFACE target
 add_library(__ck_lib INTERFACE)
 
-# specify path to shared library
-target_link_libraries(__ck_lib INTERFACE ${CMAKE_SOURCE_DIR}/torch/lib/libck_kernels.so)
+# specify path to CK library
+target_link_libraries(__ck_lib INTERFACE ${DESTINATION})
 
