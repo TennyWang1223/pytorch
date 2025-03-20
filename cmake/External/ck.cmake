f1a14cf801874fd5498f9de38b2020514b7e27b1
Akash Verma <Akash.Verma3@amd.com>
2025-03-20 23:07:17 +0000

    Use CK commit hash and ROCm version to download CK library from compute-artiactory.

--- cmake/External/ck.cmake
+++ cmake/External/ck.cmake
@@ -2,12 +2,34 @@
 # create INTERFACE target for CK library
 #
 
-# set source and destination
-set(SOURCE_URL "https://compute-artifactory.amd.com/artifactory/rocm-generic-local/torch_ck_gen_lib/ck_8086bbe3a78d931eb96fe12fdc014082e18d18d3/rocm_6.2.0/torch_2.5/libck_kernels.so")
-set(DESTINATION "${CMAKE_SOURCE_DIR}/torch/lib/libck_kernels.so")
+# get CK commit hash
+execute_process(
+	COMMAND git -C ${CMAKE_SOURCE_DIR}/third_party submodule status composable_kernel 
+	RESULT_VARIABLE result 
+	OUTPUT_VARIABLE submodule_status 
+	ERROR_VARIABLE submodule_status_error 
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+if(result EQUAL 0)
+	string(REGEX REPLACE "^[ \t]" "" submodule_status ${submodule_status})
+	# extract first 8 characters of the commit hash
+	string(SUBSTRING "${submodule_status}" 0 8 ck_commit_hash)
+else()
+	message(FATAL_ERROR "Failed to get submodule status for composable_kernel.")
+endif()
+
+# get ROCm version from LoadHIP.cmake
+include(${CMAKE_SOURCE_DIR}/cmake/public/LoadHIP.cmake)
+
+# full path for CK library on compute-artifactory.amd.com
+set(url "https://compute-artifactory.amd.com/artifactory/rocm-generic-local")
+set(ck_lib_full_path "${url}/torch_ck_gen_lib/ck_${ck_commit_hash}/${ROCM_VERSION_DEV}/libck_kernels.so")
+
+# set destination
+set(destination "${CMAKE_SOURCE_DIR}/torch/lib/libck_kernels.so")
 
 # download CK library
-file(DOWNLOAD ${SOURCE_URL} ${DESTINATION} SHOW_PROGRESS STATUS download_status)
+file(DOWNLOAD ${ck_lib_full_path} ${destination} SHOW_PROGRESS RESULT_VARIABLE download_status)
 if(NOT download_status)
 	message(STATUS "Downloaded CK library successfully.")
 else()
@@ -18,5 +40,5 @@ endif()
 add_library(__ck_lib INTERFACE)
 
 # specify path to CK library
-target_link_libraries(__ck_lib INTERFACE ${DESTINATION})
+target_link_libraries(__ck_lib INTERFACE ${destination})
 
